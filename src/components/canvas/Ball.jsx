b3991ae735b869d9f4b9ff156f62b5eaec168adf
sarthakroongta <sarthakroongta@gmail.com>
2025-07-06 01:47:44 -0400

    Added 3D Balls Skeletons

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, {Suspense} from 'react'
+import { Canvas } from '@react-three/fiber'
+import { Decal, Float, OrbitControls, Preload, useTexture } from '@react-three/drei'
 
 const Ball = () => {
   return (
