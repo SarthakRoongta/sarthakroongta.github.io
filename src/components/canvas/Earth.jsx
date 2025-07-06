531c9df934fb50f4dd050c2b534cbe989c1266b2
sarthakroongta <sarthakroongta@gmail.com>
2025-07-06 14:05:06 -0400

    Fixed error where Earth file was not being found by Github pages

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -5,10 +5,13 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei"
 import CanvasLoader from "../Loader"
 
 const Earth = () => {
-  const earth = useGLTF('./planet/scene.gltf')
+  const earth = useGLTF('/planet/scene.gltf')
   return (
     <primitive 
       object={earth.scene}
+      scale={2.5}
+      position-y={0}
+      rotation-y={0}
     />
 
   )
@@ -20,7 +23,12 @@ const EarthCanvas = () => {
       shadows
       frameloop="demand"
       gl={{preserveDrawingBuffer: true}}
-      camera = {{ }}
+      camera = {{ 
+        fov: 45,
+        near: 0.1,
+        far: 200,
+        position: [-4,3,6]
+      }}
     >
       <Suspense fallback= {<CanvasLoader />}>
         <OrbitControls
