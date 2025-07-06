acbbec7665eb6f878d66b4670912fdfc6d15f140
sarthakroongta <sarthakroongta@gmail.com>
2025-07-05 20:03:07 -0400

    Fixed ThreeJS Model spacing and page sometimes not loading

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+// src/components/Loader.jsx
+import { Html, useProgress } from "@react-three/drei";
 
-const Loader = () => {
+const CanvasLoader = () => {
+  const { progress } = useProgress();
   return (
-    <div>Loader</div>
-  )
-}
+    <Html center>
+      <span className="canvas-loader" />               {/* spinner div */}
+      <p style={{ color: "#fff", marginTop: 40 }}>
+        {progress.toFixed(0)} %
+      </p>
+    </Html>
+  );
+};
 
-export default Loader
+export default CanvasLoader;
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,41 +1,48 @@
-import {Suspense, useEffect, useState} from 'react'
-import { Canvas } from '@react-three/fiber'
-import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
+import { Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
+import CanvasLoader from '../Loader';
 
-import CanvasLoader from '../Loader'
 const Computers = () => {
-  const computer = useGLTF('./desktop_pc/scene.gltf')
+  const { scene } = useGLTF('/desktop_pc/scene.gltf');   // absolute path
+
   return (
-    <mesh>
-      <hemisphereLight intensity={0.15}
-      groundColor={"black"}/>
+    <>
+      <hemisphereLight intensity={0.15} groundColor="black" />
       <pointLight intensity={1} />
-      <primitive 
-      object={computer.scene}
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
       />
-    </mesh>
-  )
-}
+      <primitive
+        object={scene}
+        position={[-3.5, -4.25, -2.5]}
+        rotation={[-0.01, -0.2, -0.1]}
+      />
+    </>
+  );
+};
 
-const ComputersCanvas = () =>{
-  return(
-    <Canvas
-    frameloop='demand'
+const ComputersCanvas = () => (
+  <Canvas
     shadows
-    camera={{position: [20,3,5], fov: 25}}
-    gl={{preserveDrawingBuffer: true}}
-    > 
-      <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls 
-        enableZoom={false} 
-        maxPolarAngle={Math.PI /2}
-        minPolarAngle={Math.PI/2}
-        />
-        <Computers />
-      </Suspense>
-      <Preload all/>
-    </Canvas>
-  )
-}
+    camera={{ position: [20, 3, 5], fov: 25 }}
+    gl={{ preserveDrawingBuffer: true }}
+  >
+    <Suspense fallback={<CanvasLoader />}>
+      <OrbitControls
+        enableZoom={false}
+        maxPolarAngle={Math.PI / 2}
+        minPolarAngle={Math.PI / 2}
+      />
+      <Computers />
+    </Suspense>
+    <Preload all />
+  </Canvas>
+);
 
-export default ComputersCanvas
+export default ComputersCanvas;
