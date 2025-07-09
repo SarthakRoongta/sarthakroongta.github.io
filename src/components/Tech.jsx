fef8f163b5ec9c2753ae5002854cfccafb6fe935
sarthakroongta <sarthakroongta@gmail.com>
2025-07-08 20:26:31 -0400

    Changed technology balls to pokeballs

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,15 +1,17 @@
-import React from "react";
-
+import React, { useContext } from "react";
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
+import { ThemeContext } from "../theme/ThemeContext";
 
 const Tech = () => {
+  const { pokeMode } = useContext(ThemeContext);
+
   return (
     <div className='flex flex-row flex-wrap justify-center gap-10'>
       {technologies.map((technology) => (
         <div className='w-28 h-28' key={technology.name}>
-          <BallCanvas icon={technology.icon} />
+          <BallCanvas icon={technology.icon} pokeMode={pokeMode} />
         </div>
       ))}
     </div>
--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,3 +1,4 @@
+// BallCanvas.jsx
 import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import {
@@ -10,48 +11,124 @@ import {
 
 import CanvasLoader from "../Loader";
 
-const Ball = (props) => {
-  const [decal] = useTexture([props.imgUrl]);
+const Ball = ({ imgUrl, pokeMode }) => {
+  const [decal] = useTexture([imgUrl]);
 
   return (
     <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
-      <ambientLight intensity={0.25} />
-      <directionalLight position={[0, 0, 0.05]} />
-      <mesh castShadow receiveShadow scale={2.75}>
-        <icosahedronGeometry args={[1, 1]} />
-        <meshStandardMaterial
-          color='#fff8eb'
-          polygonOffset
-          polygonOffsetFactor={-5}
-          flatShading
-        />
-        <Decal
-          position={[0, 0, 1]}
-          rotation={[2 * Math.PI, 0, 6.25]}
-          scale={1}
-          map={decal}
-          flatShading
-        />
-      </mesh>
+      <ambientLight intensity={0.4} />
+      <directionalLight position={[0, 0, 0.1]} />
+
+      {/* Standard ball when not in pokeMode */}
+      {!pokeMode && (
+        <mesh castShadow receiveShadow scale={2.75}>
+          <icosahedronGeometry args={[1, 1]} />
+          <meshStandardMaterial
+            color="#fff8eb"
+            polygonOffset
+            polygonOffsetFactor={-5}
+            flatShading
+          />
+          <Decal
+            position={[0, 0, 1]}
+            rotation={[2 * Math.PI, 0, 6.25]}
+            scale={0.9}
+            map={decal}
+            flatShading
+          />
+        </mesh>
+      )}
+
+      {/* Pokéball in pokeMode */}
+      {pokeMode && (
+        <>
+          {/* Top hemisphere (red) with logo decal placed above the ring */}
+          <mesh castShadow receiveShadow scale={2.75}>
+            <sphereGeometry
+              args={[
+                1,           // radius
+                64,          // widthSegments
+                64,          // heightSegments
+                0,           // phiStart
+                2 * Math.PI, // phiLength
+                0,           // thetaStart (north pole)
+                Math.PI / 2, // down to equator
+              ]}
+            />
+            <meshStandardMaterial
+              color="#f73322"
+              polygonOffset
+              polygonOffsetFactor={-5}
+              flatShading
+            />
+            {/* move logo up onto the red half (y = 0.5, z ≈ 0.866 sits on the sphere surface) */}
+            <Decal
+              position={[0, 0.5, 0.866]}
+              rotation={[2 * Math.PI, 0, 6.25]}
+              scale={0.9}
+              map={decal}
+              flatShading
+              polygonOffset
+              polygonOffsetFactor={10}
+              renderOrder={2}
+            />
+          </mesh>
+
+          {/* Bottom hemisphere (white) */}
+          <mesh castShadow receiveShadow scale={2.75}>
+            <sphereGeometry
+              args={[
+                1,
+                64,
+                64,
+                0,
+                2 * Math.PI,
+                Math.PI / 2,  // start at equator
+                Math.PI / 2,  // down to south pole
+              ]}
+            />
+            <meshStandardMaterial
+              color="#ffffff"
+              polygonOffset
+              polygonOffsetFactor={-5}
+              flatShading
+            />
+          </mesh>
+
+          {/* Black equator ring */}
+          <mesh
+            position={[0, 0, 0]}
+            rotation={[Math.PI / 2, 0, 0]}
+            scale={2.75}
+            renderOrder={1}
+          >
+            <torusGeometry args={[1.01, 0.07, 16, 100]} />
+            <meshStandardMaterial color="#000000" />
+          </mesh>
+
+          {/* Pokéball button */}
+          <mesh position={[0, 0, 1.01]} scale={2.75} renderOrder={1}>
+            <circleGeometry args={[0.12, 32]} />
+            <meshStandardMaterial color="#000000" />
+          </mesh>
+          <mesh position={[0, 0, 1.02]} scale={2.75} renderOrder={1}>
+            <circleGeometry args={[0.06, 32]} />
+            <meshStandardMaterial color="#ffffff" />
+          </mesh>
+        </>
+      )}
     </Float>
   );
 };
 
-const BallCanvas = ({ icon }) => {
-  return (
-    <Canvas
-      frameloop='demand'
-      dpr={[1, 2]}
-      gl={{ preserveDrawingBuffer: true }}
-    >
-      <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls enableZoom={false} />
-        <Ball imgUrl={icon} />
-      </Suspense>
-
-      <Preload all />
-    </Canvas>
-  );
-};
+const BallCanvas = ({ icon, pokeMode }) => (
+  <Canvas frameloop="demand" dpr={[1, 2]} gl={{ preserveDrawingBuffer: true }}>
+    <Suspense fallback={<CanvasLoader />}>
+      <OrbitControls enableZoom={false} />
+      <Ball imgUrl={icon} pokeMode={pokeMode} />
+    </Suspense>
+    <Preload all />
+  </Canvas>
+);
 
 export default BallCanvas;
